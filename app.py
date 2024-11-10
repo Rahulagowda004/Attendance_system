12d640d6207d9d21f1cdef1c14be38de1e8f5f3a
rahulagowda277 <rahulgowda277@gmail.com>
2024-11-10 14:19:08 +0530

    everything is working just fine im just done with app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from PIL import Image
 import os
 import io
+import cv2
+import numpy as np
 from ultralytics import YOLO
 from supervision import Detections
 
@@ -12,43 +14,41 @@ class ImageCropper:
     def __init__(self, output_dir="artifacts/inference"):
         self.output_dir = output_dir
         os.makedirs(self.output_dir, exist_ok=True)
-
+        
     def crop_and_save_images(self, image, image_name):
-        # Run YOLO detection on the image
         output = model(image)
         detections = Detections.from_ultralytics(output[0])
-
-        # Save each cropped bounding box region
         for box_id, box in enumerate(detections.xyxy):
-            x1, y1, x2, y2 = map(int, box)  # Convert coordinates to integers
-
-            # Crop the image using the bounding box coordinates
+            x1, y1, x2, y2 = map(int, box)
             cropped_image = image.crop((x1, y1, x2, y2))
 
-            # Save the cropped image with a unique filename
             cropped_image_filename = f"{os.path.splitext(image_name)[0]}_box_{box_id}.png"
             cropped_image_path = os.path.join(self.output_dir, cropped_image_filename)
             cropped_image.save(cropped_image_path)
+            
+    def detect_faces(self,image):
+            output = model(image)
+            face_boxes = []
+            for result in output[0].boxes:
+                if result.cls == 0:
+                    x1, y1, x2, y2 = result.xyxy[0].tolist()
+                    face_boxes.append((int(x1), int(y1), int(x2), int(y2)))
+            image_np = np.array(image)
+            for (x1, y1, x2, y2) in face_boxes:
+                cv2.rectangle(image_np, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            final_image = Image.fromarray(image_np)
+            return final_image
 
 def main():
     st.title("YOLO Image Detection and Cropping")
 
-    # Create a file uploader widget
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        # Display the uploaded image
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_container_width=True)  # Updated here
-        
-        # Display image details
-        st.write("Image Info:")
-        st.write(f"Format: {image.format}")
-        st.write(f"Size: {image.size}")
-        st.write(f"Mode: {image.mode}")
-        
-        # Initialize ImageCropper and save cropped images
         cropper = ImageCropper()
+        predicted_image = cropper.detect_faces(image)
+        st.image(predicted_image, caption='Uploaded Image', use_container_width=True) 
         cropper.crop_and_save_images(image, uploaded_file.name)
 
 if __name__ == "__main__":
